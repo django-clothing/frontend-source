5bd103cceb1ab2b926272183c46b64fdb7c6bf64
Rosario Polito <rosario.polito@gmx.de>
2021-03-26 22:59:13 +0100

    feat: optimized sidebar

--- dist/js/navigation.js
+++ dist/js/navigation.js
@@ -1,17 +1,24 @@
-function setNavHidden(element){
-    element.classList.remove("nav");
-    element.classList.add("nav--hidden");
+function setNavHidden(nav, close){
+    nav.classList.remove("nav");
+    nav.classList.add("nav--hidden");
+
+    close.classList.remove("nav--close-button");
+    close.classList.add("nav--hidden");
 }
 
-function setNav(element){
-    element.classList.remove("nav--hidden");
-    element.classList.add("nav");
+function setNav(nav, close){
+    nav.classList.remove("nav--hidden");
+    nav.classList.add("nav");
+
+    close.classList.remove("nav--hidden");
+    close.classList.add("nav--close-button");
 }
 
 function toggleNav(){
     const nav = document.getElementById("nav-sidebar");
-    
+    const closeButton = document.getElementById("nav-close");
+
     const classname = nav.className;
-    classname === "nav--hidden" ? setNav(nav):setNavHidden(nav);
+    classname === "nav--hidden" ? setNav(nav,closeButton):setNavHidden(nav,closeButton);
 }
 
