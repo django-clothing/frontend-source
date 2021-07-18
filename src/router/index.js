8246a3c5f3542c12fa73a32f74bc3f37506d837b
Rosario-Francesco Polito <rosario.polito@gmx.de>
2021-07-18 11:54:45 +0200

    feat: reformating router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,48 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Collection from '../views/Collection.vue'
-import Cart from '../views/Cart.vue'
-import Lookbook from '../views/Lookbook.vue'
-import Product from '../views/Product.vue'
+import {createRouter, createWebHashHistory} from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Collection from '../views/Collection.vue';
+import Cart from '../views/Cart.vue';
+import Lookbook from '../views/Lookbook.vue';
+import Product from '../views/Product.vue';
 
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/collection',
-    name: 'Collection',
-    component: Collection
-  },
-  {
-    path: '/cart',
-    name: 'Cart',
-    component: Cart
-  },
-  {
-    path: '/lookbook',
-    name: 'Lookbook',
-    component: Lookbook
-  },
-  {
-    path: '/product/:id',
-    name: 'Product',
-    component: Product
-  }
-]
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/collection',
+        name: 'Collection',
+        component: Collection
+    },
+    {
+        path: '/cart',
+        name: 'Cart',
+        component: Cart
+    },
+    {
+        path: '/lookbook',
+        name: 'Lookbook',
+        component: Lookbook
+    },
+    {
+        path: '/product/:id',
+        name: 'Product',
+        component: Product
+    }
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+    history: createWebHashHistory(),
+    routes
+});
 
-export default router
+export default router;
